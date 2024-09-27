4b8d28274d3a08f2bebdb2b667682034f79afaca
BaoGiaDev18 <baodmgse182883@fpt.edu.vn>
2024-09-27 23:00:59 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showMessage() {
-  document.getElementById("message").textContent = "Cảm ơn bạn đã tha thứ!";
-}
-
-function moveButton(button) {
-  const x = Math.random() * 200 - 100;
-  const y = Math.random() * 200 - 100;
-  button.style.transform = `translate(${x}px, ${y}px)`;
-}
